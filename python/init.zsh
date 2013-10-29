2c2f9b70fe18ad04a17a385a80cc3b933d98c9f1
Mike Meyer <mikemeyer@gmail.com>
2013-10-29 16:28:06 -0700

    *yawn*

--- python/init.zsh
+++ python/init.zsh
@@ -2,6 +2,6 @@ eval "$(pyenv init -)"
 export WORKON_HOME="$HOME/.virtualenvs"
 
 VIRTUAL_ENV_DISABLE_PROMPT=1
-pyenv virtualenvwrapper_lazy
+# pyenv virtualenvwrapper_lazy
 
 alias py='python'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias z='. ~/.zshrc' # reload! is expanding to reload\! for some reason.
 alias md='mkdcd' # mkdcd comes with prezto
+alias cask='brew cask'
