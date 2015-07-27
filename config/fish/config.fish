718d157b75daff5f4def29cb482f64a7a3edacdc
Mike Meyer <github@meyer.fm>
2015-07-27 00:06:53 -0700

    hmmmm

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,3 +7,16 @@ set PATH "$HOME/.pyenv/bin" $PATH
 set PATH "$HOME/.rbenv/bin" $PATH
 set PATH "$HOME/Development/android-sdk-macosx/platform-tools" $PATH
 set PATH "$HOME/Development/arcanist/bin" $PATH
+set EDITOR "mate -w"
+
+set fish_color_autosuggestion "555"
+set fish_color_command "af87ff"
+set fish_color_comment "8a8a8a"
+set fish_color_end "ffff5f"
+set fish_color_error "ff5f5f"
+set fish_color_host "\x2do\x1ecyan"
+set fish_color_param "00afff"
+set fish_color_quote "ffaf00"
+set fish_color_redirection "00ffaf"
+set fish_color_status "red"
+set fish_color_user "\x2do\x1egreen"
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -5,6 +5,7 @@ function fish_prompt
 	# set_color $fish_color_cwd
 	set_color cyan
 	echo -n (prompt_pwd)
+	set_color normal
 	echo -n (__fish_git_prompt)
 	set_color green
 	echo -n " ❯ "
