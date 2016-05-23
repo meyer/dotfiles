37966575266de2b9f599d92da52a8ae34cb9c5fd
Mike Meyer <github.com@meyer.fm>
2016-05-23 13:52:05 -0700

    lil bits

--- symlinks/config/fish/functions.symlink/unicorn.fish
+++ symlinks/config/fish/functions.symlink/unicorn.fish
@@ -1,17 +1,15 @@
 # Requires boxes and toilet (brew install boxes toilet)
 function unicorn
-    set -lx argLen (count $argv)
+  set -lx argLen (count $argv)
   switch $argLen
-  case 1
+  case 0 1
     shout "u fucked up"
   case "*"
-    # if [eval $argv[1] = "say"] or [eval $argv[1] = "think"]
+    switch $argv[1]
+    case say think
       shout $argv[2..-1] | boxes -a c -d "unicorn$argv[1]" | toilet --gay -t -f term
-    # else
-    #   shout "param 1 needs to be shout or think."
-    # end
+    case "*"
+      shout "u fucked up again"
+    end
   end
 end
-
-# unicorn say test hello wow
-# unicorn think test string here
